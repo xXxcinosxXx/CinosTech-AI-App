e036f4de92e8a5ea8f3cea8efe858cd30f34aa09
Your Name <you@example.com>
2023-03-28 08:49:17 -0400

    update the server url

--- client/script.js
+++ client/script.js
@@ -93,7 +93,7 @@ const handleSubmit = async (e) => {
 
  // fetch data from derver -> bot's response
 
- const response = await fetch ('http://localhost:5000', {
+ const response = await fetch ('https://cinostech-a-i-app.onrender.com', {
    method: 'POST',
    headers: {
     'Content-Type': 'application/json'
